19254f5a0e9e6fe5019ea5492f099e470fba84ca
Russell Waring <russell.waring@dcmail.ca>
2023-02-10 10:57:10 -0500

    added beginning to a welcome, home, and settings screen

---
+++ lib/pages/welcome.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+/*
+author:   Katherine Bellman, Russell Waring
+version:  1
+since:    2022-02-10
+ */
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+
+      ),
+    );
+  }
+}
