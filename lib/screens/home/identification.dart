7b60316951e18d12e365e14077c5cde8732399f8
Russell Waring <russell.waring@dcmail.ca>
2023-03-19 19:51:38 -0400

    Successfully returning latin name of bird in captured image

--- lib/screens/home/identification.dart
+++ lib/screens/home/identification.dart
@@ -8,14 +8,76 @@
 import 'package:flutter/material.dart';
 import 'package:birdnerd/shared/globals.dart' as globals;
 import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
+import 'dart:async';
+import 'package:path/path.dart';
+import 'dart:convert';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter/services.dart';
 
-class IdentificationScreen extends StatelessWidget {
-  //final String imagePath;
+class IdentificationScreen extends StatefulWidget {
+  const IdentificationScreen({Key? key}) : super(key: key);
 
+  @override
+  State<IdentificationScreen> createState() => _IdentificationScreenState();
+}
+
+class _IdentificationScreenState extends State<IdentificationScreen> {
+
+  // Import the imagePath of the captured image from the globals library
   final String imagePath = globals.filepath;
+  // Identified bird name is passed to this variable
+  late String _scientificName;
+  // Used for loading state before result is identified
+  late bool _isLoading = true;
 
-  //const IdentificationScreen({super.key, required this.imagePath});
-  IdentificationScreen({Key? key}) : super(key: key);
+  /// Override initialize state method to immediately call for image recognition
+  @override
+  void initState() {
+    super.initState();
+    _getScientificName();
+  }
+
+  Future<void> _getScientificName() async {
+    final imageFile = File(imagePath);
+    await getScientificName(imageFile);
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  /// getScientificName(File imageFile)
+  /// imageFile: absolute path of captured image
+  /// Method that sends post request of image to RapidAPI Bird-Classifier and
+  /// returns JSON result set.
+  Future<void> getScientificName(File imageFile) async {
+    final url = Uri.parse('https://bird-classifier.p.rapidapi.com/BirdClassifier/prediction?results=1');
+    final request = http.MultipartRequest('POST', url)
+      ..headers.addAll({
+        'Content-Type': 'multipart/form-data; boundary=---011000010111000001101001',
+        'X-Rapidapi-Key': 'cbe9ca80d5msh13ad95efa239d2bp1a09fejsna8ba16c0268d',
+        'X-Rapidapi-Host': 'bird-classifier.p.rapidapi.com',
+      });
+    final fileStream = http.ByteStream(imageFile.openRead());
+    final fileLength = await imageFile.length();
+    final multipartFile = http.MultipartFile('image', fileStream, fileLength, filename: imageFile.path);
+
+    request.files.add(multipartFile);
+
+    final response = await request.send();
+    final responseData = await response.stream.bytesToString();
+    final decodedData = jsonDecode(responseData);
+    Map<String, dynamic> birdPrediction = decodedData[0];
+    print('Wow, it\s a ${birdPrediction['scientificName']}!');
+    // print(decodedData[0].toString());
+    _scientificName = birdPrediction['scientificName'];
+
+    /// TODO: compare _scientificName with names in database, return common name and image
+
+
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,8 +120,7 @@ class IdentificationScreen extends StatelessWidget {
                 color: Colors.teal,
                 padding: const EdgeInsets.all(15.0),
                 /// Using hardcoded image for testing purposes
-                //child: Image.file(File(imagePath)),
-                child: Image.network('https://upload.wikimedia.org/wikipedia/commons/5/5c/Male_northern_cardinal_in_Central_Park_%2852612%29.jpg'),
+                child: Image.file(File(imagePath)),
               ),
             ),
             const SizedBox(height:30.0),
@@ -69,16 +130,8 @@ class IdentificationScreen extends StatelessWidget {
                 margin: const EdgeInsets.all(10),
                 width: double.infinity,
                 color: Colors.teal,
-                child: TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    'AI Suggested Image',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      decoration: TextDecoration.underline,
-                      color: Colors.blueAccent,
-                    ),
-                  ),
+                child: Center(
+                  child: _isLoading ? CircularProgressIndicator() : Text(_scientificName),
                 ),
               ),
             ),
--- lib/shared/globals.dart
+++ lib/shared/globals.dart
@@ -7,6 +7,7 @@
 
 library globals;
 import 'package:location/location.dart';
+import 'dart:io';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 // Camera variables
@@ -15,6 +16,7 @@ double? latitude;
 double? longitude;
 
 LatLng location = LatLng(43.941990, -78.894478);
+
 
 // // Marker test
 // Map<String, Marker> markers = {
